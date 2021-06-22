1a9c41d6a7e816da1726ec188b51a9c5e7c06d90
Simon <simongussenhoven@gmail.com>
2021-06-22 17:20:06 +0200

    Reconnecting after pc wipe, also, small changes

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -7,50 +7,42 @@ function Courses() {
             </div>
             <CardGroup className="container mt-5 text-center">
                 <Card className="m-2 shadow">
-                    <div className="p-3">
                         <Card.Img variant="top" src="../images/winc.png" className="p-5" />
-                    </div>
                     <Card.Body className="justify-content-center d-flex flex-column">
                         <Card.Title>Front End Development</Card.Title>
                         <Card.Subtitle className="mb-2 text-muted text-center">Winc Academy</Card.Subtitle>
                         <Card.Text>In March 2021 I completed the front-end development course at Winc Academy, covering HTML, CSS, Javascript and React.js</Card.Text>
-                        <small className="text-muted text-center mb-3">Completed March 2021</small>
-                        <Button className="d-inline-block align-bottom">View certificate</Button>
+                        <small className="text-muted text-center mb-2 mt-auto">Completed March 2021</small>
+                        <Button>View certificate</Button>
                     </Card.Body>
                 </Card>
                 <Card className="m-2 shadow">
-                    <div className="p-3">
                         <Card.Img variant="top" src="../images/vue.png" className="p-5" />
-                    </div>
                     <Card.Body className="justify-content-center d-flex flex-column">
                         <Card.Title className="text-center">Vue.js Course</Card.Title>
                         <Card.Subtitle className="mb-2 text-muted text-center">Codecademy</Card.Subtitle>
                         <Card.Text>During the start of 2021 I took the Vue.js course on codecademy, looking forward to experimenting with it.</Card.Text>
-                        <small className="text-muted text-center mb-3">Completed March 2021</small>
-                        <Button className="d-inline-block align-bottom">View certificate</Button>
+                        <small className="text-muted text-center mt-auto mb-2">Completed March 2021</small>
+                        <Button>View certificate</Button>
                     </Card.Body>
                 </Card>
                 <Card className="m-2 shadow">
-                    <div className="p-3">
                         <Card.Img variant="top" src="../images/bootstrap.png" className="p-5" />
-                    </div>
                     <Card.Body className="justify-content-center d-flex flex-column">
                         <Card.Title className="text-center">Bootstrap Course</Card.Title>
                         <Card.Subtitle className="mb-2 text-muted text-center">Codecademy</Card.Subtitle>
                         <Card.Text>The Bootstrap course of Codecademy comes in handy, althoug react-bootstrap (used in this page) is slightly different.</Card.Text>
-                        <small className="text-muted text-center mb-3">Completed March 2021</small>
-                        <Button className="d-inline align-bottom">View certificate</Button>
+                        <small className="text-muted text-center mt-auto mb-2">Completed March 2021</small>
+                        <Button>View certificate</Button>
                     </Card.Body>
                 </Card>
                 <Card className="m-2 shadow">
-                    <div className="p-3">
                         <Card.Img variant="top" src="../images/jquery.png" className="p-5" />
-                    </div>
                     <Card.Body className="justify-content-center d-flex flex-column">
                         <Card.Title className="text-center">jQuery Course</Card.Title>
                         <Card.Subtitle className="mb-2 text-muted text-center">Codecademy</Card.Subtitle>
                         <Card.Text>I've done the jQuery course earlier, but took a little refresh in march 2021.</Card.Text>
-                        <small className="text-muted text-center mb-3">Completed March 2021</small>
+                        <small className="text-muted text-center mt-auto mb-2">Completed March 2021</small>
                         <Button>View certificate</Button>
                     </Card.Body>
                 </Card>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,20 @@
+import { Button } from "react-bootstrap";
 function Header() {
     return (
         <header className="bg-dark vh-100 d-flex align-items-center justify-content-center masthead">
-            <div className="masthead__heading">
-                <h1 className="display-1 text-light text-center">Hi! I'm Simon</h1>
-                <h2 className="display-3 text-light text-center">Aspiring front end developer</h2>
+            <div className="masthead__heading d-flex flex-column">
+                <div className="row d-flex flex-column mx-2">
+                    <h1 className="display-1 text-light text-center">Hi! I'm Simon</h1>
+                    <h2 className="display-3 text-light text-center">Aspiring front end developer</h2>
+                </div>
+                <div className="row d-flex justify-content-center my-5">
+                    <div className="col-4 col-md-2">
+                        <Button className="btn w-100">Contact</Button>
+                    </div>
+                    <div className="col-4 col-md-2">
+                        <Button className="btn w-100 text-nowrap">About me</Button>
+                    </div>  
+                </div>
             </div>
         </header>
     );
